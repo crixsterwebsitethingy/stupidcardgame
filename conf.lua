6287104d12406dfef6aedd8defc75d72615a8dd8
crix_0z <crixsteryt@gmail.com>
2026-01-06 17:14:41 +0200

    v1.0 alpha development

---
+++ conf.lua
@@ -0,0 +1,6 @@
+function love.conf(t)
+    t.window.resizable = true
+    t.window.vsync = true
+    t.window.fullscreen = false
+    t.window.title = "A Stupid Card Game v1.0 alpha"
+end
---
+++ main.lua
@@ -0,0 +1,15 @@
+x = 1
+y = 1
+function love.load()
+    local fontmenu = love.graphics.newFont("assets/Montserrat-Regular.ttf", 24)
+    love.graphics.setFont(fontmenu)
+end
+function love.update(dt)
+
+end
+
+function love.draw()
+    love.graphics.print("A Stupid Card Game", love.graphics.getWidth() / 2 - 120, 20, 0, 1, 1)
+    love.graphics.setBackgroundColor(0, 0.4, 0.5)
+    love.graphics.rectangle("fill", 20, 70 , love.graphics.getWidth() - 40 , 50)
+end
