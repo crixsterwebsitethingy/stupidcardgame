f0ad97e9729a810b94884613e3164ef36f83a253
crix_0z <crixsteryt@gmail.com>
2026-01-06 17:41:30 +0200

    game logic dev start

--- game.lua
+++ game.lua
@@ -1,2 +1,16 @@
 -- © 2026 crix_0z, All Rights Reserved.
 -- This code is licensed under the MIT License.
+local game = {}
+game.__index = game
+function game:new()
+    local instance = setmetatable({}, game)
+    instance.state = "menu"
+    instance.hand = {}
+    instance.score = 0
+    instance.target = 0
+    instance.jokers = {}
+    return instance
+end
+
+
+return game
